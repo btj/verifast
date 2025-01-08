0c3ac1664e5114ceb6d2e4b5736e577374ce26fc
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2025-01-08 14:02:39 +0100

    Simply use Invoke-WebRequest on the asset URL

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,13 +1,4 @@
-$downloadFolder = "C:\temp";
-$ownerSlashRepo = "ocaml/opam";
-$tag = "2.3.0";
-
-$json = Invoke-Webrequest -Uri "https://api.github.com/repos/$ownerSlashRepo/releases/$tag" -Headers @{'Accept'='application/json'}
-$release = $json.Content | ConvertFrom-Json
-$release.assets | ?{$_.name -eq 'opam-2.3.0-x86_64-windows.exe'} | %{
-    $asset = $_;
-    $x = Invoke-Webrequest -Uri $($asset.url) -OutFile "$downloadFolder\$($asset.name)" -Headers @{'Accept'='application/octet-stream'}
-}
+Invoke-Webrequest -Uri "https://github.com/ocaml/opam/releases/download/2.3.0/opam-2.3.0-x86_64-windows.exe" -OutFile "C:\temp\opam.exe"
 
 C:\temp\opam init -y
 C:\temp\opam install lablgtk

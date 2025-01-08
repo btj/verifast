d991b80d886bb4381db3241852193d1d656f9e28
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2025-01-08 14:20:28 +0100

    opam install -y

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,4 +1,4 @@
 Invoke-Webrequest -Uri "https://github.com/ocaml/opam/releases/download/2.3.0/opam-2.3.0-x86_64-windows.exe" -OutFile "C:\temp\opam.exe"
 
 C:\temp\opam init -y
-C:\temp\opam install lablgtk
+C:\temp\opam install -y lablgtk
